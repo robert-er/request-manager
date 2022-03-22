98cc6bd8b54a151f5e6967912790b4a273f09a53
robert-er <robert.roza.pl@gmail.com>
2022-03-22 23:30:29 +0100

    added gradle basic project structure

---
+++ src/test/java/com/request/manager/ManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.request.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
